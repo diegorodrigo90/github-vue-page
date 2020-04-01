c4b82fb05d08092e798233302ca5d97223933980
Diego Rodrigo <diegorodrigo90@gmail.com>
2020-04-01 12:06:55 -0300

    prismic

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import PrismicVue from 'prismic-vue'
+import linkResolver from './prismic/link-resolver'
+import htmlSerializer from './prismic/html-serializer'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(PrismicVue, {
+  endpoint: window.prismic.endpoint,
+  linkResolver,
+  htmlSerializer
+})
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
